f93e5a7f6a83401cdafbd05100537ade0cc8f6f0
Ralamboniaina <ralamboniainafano@gmail.com>
2023-03-21 00:46:49 +0300

    Add MarsRoverNorthState, MarsRoverEastState, MarsRoverSouthState, MarsRoverWestState classes

---
+++ src/main/java/MarsRoverNorthState.java
@@ -0,0 +1,44 @@
+public class MarsRoverNorthState {
+    private int x;
+    private int y;
+    private char direction;
+
+    public MarsRoverNorthState(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public char direction() {
+        return direction;
+    }
+
+    void turnLeft() {
+        if (direction == 'N') direction = 'W';
+        else if (direction == 'W') direction = 'S';
+        else if (direction == 'S') direction = 'E';
+        else direction = 'N';
+    }
+
+    void turnRight() {
+        if (direction == 'E') direction = 'S';
+        else if (direction == 'S') direction = 'W';
+        else if (direction == 'W') direction = 'N';
+        else direction = 'E';
+    }
+
+    void moveForward() {
+        if (direction == 'N') y = y + 1;
+        else if (direction == 'S') y = y - 1;
+        else if (direction == 'W') x = x - 1;
+        else x = x + 1;
+    }
+}
